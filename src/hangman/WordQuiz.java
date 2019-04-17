e8b5010bb4f78134a4b556a06cb818125cee37c0
Daniel Marten <dmndnlrstz@gmail.com>
2019-04-17 19:50:44 +0200

    -fully implemented the project -added some TODO's for bug fixing

--- src/hangman/WordQuiz.java
+++ src/hangman/WordQuiz.java
@@ -1,18 +1,20 @@
 package hangman;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Scanner;
+import java.util.Arrays;
 
 /**
  * WordQuiz is the main class for the hangman game
  * 17.04.2019
  * @author Daniel Marten
+ * TODO: What happens if the wordlist is empty
+ * TODO: if you enter a char it will only uncover one --> can never win :(
+ * TODO: uncover is never used
  */
 public class WordQuiz {
 
     protected String quizword;
+    protected char[] guessedWord;
     protected char[] guessedLetters;
     protected int remainingAttemps;
     protected ConsoleReader consoleReader;
@@ -26,62 +28,83 @@ public class WordQuiz {
      * @param wordlistReader Wordlist Reader
      */
     public WordQuiz(int wordLength, int remainingAttemps, ConsoleReader consoleReader, WordlistReader wordlistReader){
-        //TODO: choose random word with wordLength
         this.guessedLetters = new char[remainingAttemps];
         this.remainingAttemps = remainingAttemps;
         this.consoleReader = consoleReader;
         this.wordlistReader = wordlistReader;
         this.quizword = wordlistReader.getRandomWordFromList();
+        this.guessedWord = new char[wordLength];
     }
 
     /**
      * Starts the hangman game
      */
-    public void playGame()throws IOException {
+    public void playGame() throws IOException {
 
         boolean gameEnd = false;
+        char guessedChar;
+
+        fillGuessedWOrd();
 
         System.out.println("~~~~~~~~~~~~~GAME START~~~~~~~~~~~~~~");
         System.out.println();
 
-        // Solange printGameInfo und andere Methode zur Eingabe,Vergleich bis game ende
-        while (this.remainingAttemps > 0 || false){
+        while ((this.remainingAttemps > 0) || !gameEnd){
             printGameInfo();
 
+            guessedChar = this.consoleReader.readNextChar();
+
+            if (compareInput(guessedChar) == 1){
+                for (int i = 0; i < quizword.length(); i++){
+                    if (quizword.charAt(i) == guessedChar){
+                        guessedWord[i] = guessedChar;
+                    }
+                }
+            }
+
+            if (!Arrays.toString(guessedWord).contains("_")){
+                gameEnd = true;
+                System.out.println("YOU GUESSED THE WORD !!!");
+            }
+            else if (remainingAttemps == 0){
+                gameEnd = true;
+                System.out.println("YOU HAVE TO ATTEMPS LEFT. YOU LOOSE !!!");
+            }
 
             this.remainingAttemps--;
         }
 
 
-        // Message won/lose
-        // then system.exit
-
-
-
     }
+
+
 
     /**
      * Compares the input to the quizword
      * @param character character
      * @return 1=equals, 0=not equal, //-1= error
-     * TODO: HÄLP
      */
     protected int compareInput(char character){
-        if (equals(character))
-            return 1;
-        else {
-            return 0;
+        try{
+            String search = String.valueOf(character).toLowerCase();
+            if (this.quizword.toLowerCase().contains(search))
+                return 1;
+            else {
+                return 0;
+            }
+        }
+        catch (Exception e){
+            return -1;
         }
     }
 
     /**
-     * ?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?
-     * @param character ja ne
-     * @param i is klar
-     * TODO: dafuq !?!?!?!?!?
+     * Replaces the _ at a specified index in guessedWord
+     * @param character character to replace _
+     * @param index index of the guessedWord
      */
-    protected void uncover(char character, int i){
-
+    protected void uncover(char character, int index){
+        this.guessedWord[index] = character;
     }
 
     /**
@@ -89,33 +112,32 @@ public class WordQuiz {
      */
     private void printGameInfo(){
 
-        // create and fill guessed words with _
-        String[] guessedWord = new String[this.quizword.length()];
-        for (int i = 0; i < guessedWord.length; i++){
-            guessedWord[i] = "_ ";
-        }
-
         // print remainingAttempts
         System.out.printf("Remaining Attemps: %d%n", this.remainingAttemps);
 
         //print guessedletters
-        String builder = "";
+        StringBuilder builder = new StringBuilder();
         for (char character : guessedLetters){
-            builder += character;
+            builder.append(character);
         }
-        System.out.printf("Guessed letters: %s%n", builder);
+        System.out.printf("Guessed letters: %s%n", builder.toString());
 
+        // print the current status of the guessedWord
+        builder = new StringBuilder();
+        for (char c : guessedWord) {
+            builder.append(c);
+        }
+        System.out.printf("Quizword: %s%n", builder.toString());
 
-        //MAGIC to fill and compare guessed word
-        //Maybe in (another Method) playgame
+    }
 
-
-        // print the current guessedWord
-        builder = "";
+    /**
+     * Method for filling guessedWord with _
+     */
+    private void fillGuessedWOrd(){
         for (int i = 0; i < guessedWord.length; i++){
-            builder += guessedWord[i];
+            this.guessedWord[i] = '_';
         }
-        System.out.printf("Quizword: %s%n", builder);
     }
 
 
