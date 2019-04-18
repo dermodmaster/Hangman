5a80684dbd8f9e7738391352a6995f441ad26892
Daniel Marten <dmndnlrstz@gmail.com>
2019-04-18 16:54:14 +0200

    -Added Module: Uml2Cod

---
+++ UML2CODE/src/Autor.java
@@ -0,0 +1,21 @@
+/**
+ * Class Autor: Owner of an Aenderung
+ * 18.04.2019
+ * @author Daniel Marten, Maximilian Frömelt, Ruben Klinksiek
+ */
+public class Autor {
+
+    private int personalNumber;
+    private String name;
+
+    /**
+     * Constructor for Autor
+     * @param pNumber personal Number
+     * @param name name of the Autor
+     */
+    public Autor(int pNumber, String name){
+
+    }
+
+
+}
---
+++ UML2CODE/src/FunktionaleAnforderung.java
@@ -0,0 +1,18 @@
+/**
+ * Class FunktionaleAnforderun:
+ *
+ * 18.04.2019
+ * @author Daniel Marten, Maximilian Frömelt, Ruben Klinksiek
+ */
+public class FunktionaleAnforderung extends Anforderung{
+
+    /**
+     * Constructor for FunktionaleAnforderung
+     * @param id id of the Anforderung
+     * @param text of the Anforderung
+     */
+    public FunktionaleAnforderung(int id, String text){
+
+    }
+
+}
